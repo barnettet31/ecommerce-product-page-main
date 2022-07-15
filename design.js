4ae8e0255b9ecdf599350e997f29bb14559af76f
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-07-15 15:23:07 -0400

    fixed issues with scroller and modified desktop

--- design.js
+++ design.js
@@ -3,10 +3,25 @@ const nav = document.querySelector(".nav");
 const overlay = document.querySelector(".overlay");
 const scroller = document.querySelector(".slide-div");
 const slideButtons = document.querySelectorAll(".slide-button");
+
+
+///////////////////////////MENU HANDLER ////////////////////////////////////////
 function toggleMenu() {
   nav.classList.toggle("nav--shown");
   overlay.classList.toggle("overlay--shown");
 }
+
+header.addEventListener("click", (event) => {
+  const targetClass = event.target.classList;
+  if (targetClass.contains("menu-button") || targetClass.contains("close-menu"))
+    return toggleMenu();
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////SLIDER HANDLER ////////////////////////////////////////
+
 function handleSlide(direction) {
   const currentSlide = parseInt(scroller.getAttribute("current-slide"));
   let nextSlide = currentSlide + 1;
@@ -16,11 +31,7 @@ function handleSlide(direction) {
   document.getElementById(`slide-${nextSlide}`).scrollIntoView(false);
   scroller.setAttribute("current-slide", nextSlide.toString());
 }
-header.addEventListener("click", (event) => {
-  const targetClass = event.target.classList;
-  if (targetClass.contains("menu-button") || targetClass.contains("close-menu"))
-    return toggleMenu();
-});
+
 
 scroller.addEventListener("click", (e) => {
   if (e.target.classList.contains("slide-button")) {
@@ -28,3 +39,15 @@ scroller.addEventListener("click", (e) => {
     handleSlide(targetId);
   }
 });
+
+
+
+document.querySelector('.thumb__container').addEventListener('click', ({target})=>{
+  if(!target.classList.contains('thumb_image')) return 
+  document.querySelector('.current__thumb').classList.remove('current__thumb');
+  target.classList.add('current__thumb');
+  const targetData = target.getAttribute('data-image-number');
+  document.getElementById(`slide-${targetData}`).scrollIntoView(false);
+  
+})
+
---
+++ shopping-cart.js
@@ -0,0 +1,1 @@
+//TODO create a shopping cart class that will take in an item then increment and decrement those items based on user input
