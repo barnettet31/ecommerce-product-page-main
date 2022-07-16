1737da4162240e14aa0e5868dbe8a17563e2b1a1
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-07-16 15:04:18 -0400

    finished raw html/css for shopping cart popup

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,1 +1,14 @@
 //TODO create a shopping cart class that will take in an item then increment and decrement those items based on user input
+//TODO get relevant items
+
+const cartIcon = document.querySelector('.cart');
+const cartWindow = document.querySelector('.shopping__cart_window');
+const cartNumber = document.querySelector('.item_number');
+
+
+const cartEmptyHTML = '<p class="empty-cart">Your Cart Is Empty</p>'
+const cartWithItemHTML = ({itemName, number})=>`<div>${itemName}</div>`;
+const handleShow=(e)=>{
+    cartWindow.classList.toggle('shopping_cart_window--show')
+}
+cartIcon.addEventListener('click', handleShow);
