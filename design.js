54e748f18394333f539b83ec723439d4990119c4
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-07-16 20:54:19 -0400

    finished overlay behavior

--- design.js
+++ design.js
@@ -6,6 +6,7 @@ const slideButtons = document.querySelectorAll(".slide-button");
 const slideOverlay = document.querySelector('.slide-overlay');
 const mainPictureContainer = document.querySelector('.slide-container');
 const slideOverLayClose = document.querySelector('.slide-overlay-close');
+const overlayButtonContainer = document.querySelector('.slide-overlay-button-container');
 ///////////////////////////MENU HANDLER ////////////////////////////////////////
 function toggleMenu() {
   nav.classList.toggle("nav--shown");
@@ -59,4 +60,38 @@ const showSlideOverlay = ()=>{
 mainPictureContainer.addEventListener('click', showSlideOverlay);
 slideOverLayClose.addEventListener('click', showSlideOverlay);
 
+const overLayNextPhoto = (number)=>{
+  numberToGo = number ==4? 1 : number + 1;
+  slideOverlay.setAttribute('data-current-photo-count', numberToGo.toString());
+  document.querySelector('.overlay-carousel-thumb--active').classList.remove('overlay-carousel-thumb--active');
+  document.querySelector(`#overlay-thumb-${numberToGo}`).classList.add('overlay-carousel-thumb--active');
+  document.querySelector(`#overlay-slide-${numberToGo}`).scrollIntoView(false);
+  //update active thumb
+  //scroll to image
 
+}
+const overlayPreviousPhoto = (number)=>{
+  numberToGo = number ==1?4 : number - 1;
+  slideOverlay.setAttribute('data-current-photo-count', numberToGo.toString());
+  document.querySelector('.overlay-carousel-thumb--active').classList.remove('overlay-carousel-thumb--active');
+  document.querySelector(`#overlay-thumb-${numberToGo}`).classList.add('overlay-carousel-thumb--active');
+  document.querySelector(`#overlay-slide-${numberToGo}`).scrollIntoView(false);
+
+}
+overlayButtonContainer.addEventListener('click', ({target})=>{
+  const currentPhotoNumber = Number(slideOverlay.getAttribute('data-current-photo-count'));
+  if(target.classList.contains('overlay-next-slide') || target.classList.contains('overlay-next-icon')) return overLayNextPhoto(currentPhotoNumber);
+  if(target.classList.contains('overlay-previous-slide'|| target.classList.contains('overlay-previous-icon'))) return overlayPreviousPhoto(currentPhotoNumber);
+});
+
+const overLayThumbsContainer = document.querySelector('.slide-overlay-thumb-container');
+
+const handleThumbClick =({target})=>{
+  if(!target.classList.contains('overlay-carousel-thumb')) return;
+  const newNumber = Number(target.getAttribute('data-thumb-number'));
+  document.querySelector('.overlay-carousel-thumb--active').classList.remove('overlay-carousel-thumb--active');
+  target.classList.add("overlay-carousel-thumb--active");
+  slideOverlay.setAttribute('data-current-photo-count', newNumber.toString());
+  document.querySelector(`#overlay-slide-${newNumber}`).scrollIntoView(false);
+}
+overLayThumbsContainer.addEventListener('click', handleThumbClick);
